45ee34562118eee179480d1dc70672cf430e5164
BarbaraFerlinc <barbara.ferlinc@student.um.si>
2025-01-26 16:25:32 +0100

    naloga 5

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,7 @@
 const admin = require("firebase-admin");
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const serviceAccount = JSON.parse(process.env.EXPRESS_APP_FIREBASE_KEY) || require("./key.json");
 
